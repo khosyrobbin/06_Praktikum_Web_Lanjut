2f388d064199e693873348b8932a582d2c212f80
khosy robbin <khosybin@gmail.com>
2021-03-16 11:57:40 +0700

    Praktikum 3: update web&home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,7 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $users = Auth::user();
-        return view('home',['user' => $users]);
+        $user = Auth::user();
+        return view('home',['user' => $user]);
     }
 }
