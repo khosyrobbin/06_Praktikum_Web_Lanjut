ceee8867014f10330da92274185914ce570cb367
khosy robbin <khosybin@gmail.com>
2021-03-16 08:53:53 +0700

    Praktikum 2: Edit File UseSeeder

--- database/seeders/UseSeeder.php
+++ database/seeders/UseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UseSeeder extends Seeder
 {
@@ -13,6 +15,10 @@ class UseSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            'name' => 'Administrator',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password'),
+        ]);
     }
 }
